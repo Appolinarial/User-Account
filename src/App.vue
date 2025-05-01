1d65e3cc54dab571b19d9fc8457480310e4f02ff
Паулина Лиллерт <paulinalillert@MacBook-Air-Paulina.local>
2025-05-01 15:58:02 +0300

    initialisation of the project, installation sass, creation footer, styles components

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HomePage from './pages/HomePage.vue';
+
+</script>
+
+<template>
+<HomePage></HomePage>
+</template>
+
+<style lang="sass" scoped>
+@use "./assets/styles/index" as area;
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Footer from "../components/Footer.vue"
+</script>
+
+<template>
+<Footer></Footer>
+</template>
+
+<style lang="sass" scoped>
+@use "../assets/styles/index" as main;
+</style>
