593d3d7b7ee0873ebed4ca4b91d92fe9126d8d72
Паулина Лиллерт <paulinalillert@MacBook-Air-Paulina.local>
2025-05-05 14:32:44 +0300

    updated BEM classes of FilesBlock.vue

--- src/components/Main/FilesBlock.vue
+++ src/components/Main/FilesBlock.vue
@@ -27,316 +27,293 @@ const currentDate = new Date().toLocaleDateString('ru-RU', {
 })
 </script>
 
-
 <template>
-    <section class="files-container mobile-hidden">
-      <h2 class="header">Файлы</h2>
-  
-      <div
-        v-if="filesStore.mediaplans.length === 0 && filesStore.reports.length === 0"
-        class="files-container__preview"
-      >
-        <img src="@/assets/icons/color-folders.svg" alt="folders">
-        <p>Закажи у личного помощника<br>медиаплан. Он появится в этом<br>разделе</p>
-      </div>
-  
-      <div v-else class="files-container-desctop">
-        <div class="files-container-desctop__wrapper">
-          <!-- Медиапланы -->
-          <div class="files-container-desctop__mediaplans">
-            <h3 class="files-container-desctop__mediaplans__header">Медиапланы</h3>
-            <div class="scroll-box">
-              <div
-                v-for="(item, index) in visibleMediaplans"
-                :key="item.id"
-              >
-                <Mediaplan 
+  <section class="files mobile-hidden">
+    <h2 class="files__title">Файлы</h2>
+
+    <div
+      v-if="filesStore.mediaplans.length === 0 && filesStore.reports.length === 0"
+      class="files__preview"
+    >
+      <img src="@/assets/icons/color-folders.svg" alt="folders">
+      <p>Закажи у личного помощника<br>медиаплан. Он появится в этом<br>разделе</p>
+    </div>
+
+    <div v-else class="files__desktop">
+      <div class="files__desktop-wrapper">
+        <div class="files__section">
+          <h3 class="files__section-title">Медиапланы</h3>
+          <div class="files__scroll">
+            <div
+              v-for="(item, index) in visibleMediaplans"
+              :key="item.id"
+            >
+              <Mediaplan 
                 :item="item" 
                 :index="filesStore.mediaplans.length - index" 
                 :total="filesStore.mediaplans.length" 
-            />
-                <p v-if="index === 0 && item.status" class="mediaplan-status">
-                  <img
-                    v-if="item.status === 'pending'"
-                    src="@/assets/icons/warning.svg"
-                    alt="В процессе"
-                    class="mediaplan-status__icon"
-                  />
-                  <img
-                    v-else
-                    src="@/assets/icons/done.svg"
-                    alt="Готов"
-                    class="mediaplan-status__icon"
-                  />
-                  <span>
-                    {{
-                      item.status === 'pending'
-                        ? 'Медиаплан формируется'
-                        : `Медиаплан от ${currentDate} готов`
-                    }}
-                  </span>
-                </p>
-              </div>
+              />
+              <p v-if="index === 0 && item.status" class="files__status files__status--mediaplan">
+                <img
+                  v-if="item.status === 'pending'"
+                  src="@/assets/icons/warning.svg"
+                  alt="В процессе"
+                  class="files__status-icon"
+                />
+                <img
+                  v-else
+                  src="@/assets/icons/done.svg"
+                  alt="Готов"
+                  class="files__status-icon"
+                />
+                <span>
+                  {{
+                    item.status === 'pending'
+                      ? 'Медиаплан формируется'
+                      : `Медиаплан от ${currentDate} готов`
+                  }}
+                </span>
+              </p>
             </div>
-            <div class="show-more-wrapper">
-                <button
-                v-if="filesStore.mediaplans.length > 4"
-                class="show-more-button"
-                @click="showAllMediaplans = !showAllMediaplans"
-                >
-                {{ showAllMediaplans ? 'Скрыть' : 'Показать ещё' }}
-                </button>
-        </div>
           </div>
-  
-          <!-- Отчеты -->
-          <div class="files-container-desctop__reports">
-            <h3 class="files-container-desctop__reports__header">Отчеты</h3>
-            <div class="scroll-box">
-              <div
-                v-for="(item, index) in visibleReports"
-                :key="item.id"
-              >
-                <Report 
+          <div class="files__show-more">
+            <button
+              v-if="filesStore.mediaplans.length > 4"
+              class="files__show-more-button"
+              @click="showAllMediaplans = !showAllMediaplans"
+            >
+              {{ showAllMediaplans ? 'Скрыть' : 'Показать ещё' }}
+            </button>
+          </div>
+        </div>
+
+        <div class="files__section">
+          <h3 class="files__section-title">Отчеты</h3>
+          <div class="files__scroll">
+            <div
+              v-for="(item, index) in visibleReports"
+              :key="item.id"
+            >
+              <Report 
                 :item="item" 
                 :index="filesStore.reports.length - index" 
-                :total="filesStore.reports.length" />
-                <p v-if="index === 0 && item.status" class="report-status">
-                  <img
-                    v-if="item.status === 'pending'"
-                    src="@/assets/icons/warning.svg"
-                    alt="Формируется"
-                    class="report-status__icon"
-                  />
-                  <img
-                    v-else
-                    src="@/assets/icons/done.svg"
-                    alt="Готово"
-                    class="report-status__icon"
-                  />
-                  <span>
-                    {{
-                      item.status === 'pending'
-                        ? 'Отчет формируется'
-                        : `Отчет от ${currentDate} готов`
-                    }}
-                  </span>
-                </p>
-              </div>
+                :total="filesStore.reports.length" 
+              />
+              <p v-if="index === 0 && item.status" class="files__status files__status--report">
+                <img
+                  v-if="item.status === 'pending'"
+                  src="@/assets/icons/warning.svg"
+                  alt="Формируется"
+                  class="files__status-icon"
+                />
+                <img
+                  v-else
+                  src="@/assets/icons/done.svg"
+                  alt="Готово"
+                  class="files__status-icon"
+                />
+                <span>
+                  {{
+                    item.status === 'pending'
+                      ? 'Отчет формируется'
+                      : `Отчет от ${currentDate} готов`
+                  }}
+                </span>
+              </p>
             </div>
-            <div class="show-more-wrapper">
-                <button
-                v-if="filesStore.reports.length > 4"
-                class="show-more-button"
-                @click="showAllReports = !showAllReports"
-                >
-                {{ showAllReports ? 'Скрыть' : 'Показать ещё' }}
-                </button>
           </div>
+          <div class="files__show-more">
+            <button
+              v-if="filesStore.reports.length > 4"
+              class="files__show-more-button"
+              @click="showAllReports = !showAllReports"
+            >
+              {{ showAllReports ? 'Скрыть' : 'Показать ещё' }}
+            </button>
           </div>
         </div>
       </div>
-    </section>
-    <section class="desctop-hidden">
-  <div class="files-container__mobile">
-    <!-- Медиапланы -->
-    <div>
-      <Button
-        v-if="filesStore.mediaplans.length === 0"
-        class="files-container__button-mediaplan"
-        text="Медиапланы"
-      />
-      <Button
-        v-else
-        class="files-container__button-mediaplan-updated"
-        text="Медиапланы"
-      >
-      <img src="@/assets/icons/done.svg" alt="Готово" class="mediaplan-mobile-status__icon"/></Button>
     </div>
-    <!-- Отчеты -->
-    <div>
-      <Button
-        v-if="filesStore.reports.length === 0"
-        class="files-container__button-report"
-        text="Отчеты"
-      />
-      <Button
-        v-else
-        class="files-container__button-report-updated"
-        text="Отчеты"
-      />
+  </section>
+
+  <section class="files-mobile desctop-hidden">
+    <div class="files-mobile__buttons">
+      <div>
+        <Button
+          v-if="filesStore.mediaplans.length === 0"
+          class="files-mobile__button files-mobile__button--mediaplan"
+          text="Медиапланы"
+        />
+        <Button
+          v-else
+          class="files-mobile__button files-mobile__button--mediaplan files-mobile__button--updated"
+          text="Медиапланы"
+        >
+          <img src="@/assets/icons/done.svg" alt="Готово" class="files-mobile__icon"/>
+        </Button>
+      </div>
+      <div>
+        <Button
+          v-if="filesStore.reports.length === 0"
+          class="files-mobile__button files-mobile__button--report"
+          text="Отчеты"
+        />
+        <Button
+          v-else
+          class="files-mobile__button files-mobile__button--report files-mobile__button--updated"
+          text="Отчеты"
+        />
+      </div>
     </div>
-  </div>
-</section>
-  </template>
+  </section>
+</template>
 
 <style lang="scss" scoped>
-@use "@/assets/styles/index" as main;
+@use '@/assets/styles/index' as main;
 
-.desctop-hidden {
+.files {
+  width: 295px;
+  height: 709px;
+  border-radius: 20px;
+  background-color: main.$white;
+  margin-bottom: 20px;
+
+  &__title {
+    margin: 0;
+    padding: 28px 0px 0px 20px;
+  }
+
+  &__preview {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-size: 16px;
+    line-height: 145%;
+    padding-top: 46px;
+    width: 100%;
+  }
+
+  &__desktop-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__section {
+    padding: 20px;
+    font-size: 18px;
+  }
+
+  &__section-title {
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  &__scroll {
+    max-height: 200px;
+    overflow-y: auto;
+    margin-bottom: 12px;
+    padding-right: 4px;
+  }
+
+  &__status {
+    margin: 6px 0 16px 0;
+    font-size: 14px;
+    color: #555;
+    font-style: italic;
+    display: flex;
+    align-items: center;
+
+    &-icon {
+      padding-right: 5px;
+    }
+  }
+
+  &__show-more {
+    display: flex;
+    justify-content: flex-end;
+
+    &-button {
+      background: none;
+      border: none;
+      color: main.$purple;
+      cursor: pointer;
+      font-size: 14px;
+      padding: 0;
+    }
+  }
+}
+
+.files-mobile {
   display: none;
+
+  &__buttons {
+    font-size: 14px;
+    padding: 20px 0 30px 20px;
+    background-color: #fff;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+
+  &__button {
+    height: 41px;
+    background-color: main.$dark-grey;
+    border: none;
+    font-weight: bolder;
+    border-radius: 4%;
+    padding: 12px;
+    font-size: 14px;
+    text-align: center;
+    margin-right: 20px;
+
+    &--updated {
+      background-color: main.$white;
+    }
+
+    &--mediaplan {
+      border: 1px solid main.$purple;
+    }
+
+    &--report {
+      border: 1px solid main.$blue;
+    }
+  }
+
+  &__icon {
+    padding-left: 5px;
+    vertical-align: middle;
+    display: inline-block;
+  }
 }
 
 .mobile-hidden {
   display: inline;
 }
 
-.files-container {
-  width: 295px;
-  height: 709px;
-  border-radius: 20px;
-  background-color: main.$white;
-  margin-bottom: 20px;
-}
-
-.files-container__mobile {
-  font-size: 14px;
-  padding: 20px 0 30px 20px;
-  background-color: #fff;
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-}
-
-.header {
-  margin: 0;
-  padding: 28px 0px 0px 20px;
-}
-
-.files-container-desctop__mediaplans__header,
-.files-container-desctop__reports__header {
-  font-size: 18px;
-  font-weight: 600;
-}
-
-.files-container__preview {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  font-size: 16px;
-  line-height: 145%;
-  padding-top: 46px;
-  width: 100%;
-}
-
-.files-container__button-mediaplan,
-.files-container__button-report {
-  height: 41px;
-  background-color: main.$dark-grey;
-  border: none;
-  font-weight: bolder;
-  border-radius: 4%;
-  padding: 12px;
-  font-size: 14px;
-  text-align: center;
-}
-
-.files-container__button-mediaplan-updated,
-.files-container__button-report-updated {
-  height: 41px;
-  background-color: main.$white;
-  font-weight: bolder;
-  border-radius: 4%;
-  padding: 12px;
-  font-size: 14px;
-  text-align: center;
-}
-
-.files-container__button-mediaplan-updated {
-  border: 1px solid main.$purple;
-  margin-right: 20px;
-}
-
-.files-container__button-report-updated {
-  border: 1px solid main.$blue;
-}
-
-.files-container__button-mediaplan {
-  margin-right: 20px;
-}
-
-.files-container-desctop__mediaplans,
-.files-container-desctop__reports {
-  padding: 20px 20px 0 20px;
-  font-size: 18px;
-}
-
-.scroll-box {
-  max-height: 200px;
-  overflow-y: auto;
-  margin-bottom: 12px;
-  padding-right: 4px;
-}
-
-.files-container-desctop__wrapper {
-  display: flex;
-  flex-direction: column;
-}
-
-.mediaplan-status,
-.report-status {
-  margin: 6px 0 16px 0px;
-  font-size: 14px;
-  color: #555;
-  font-style: italic;
-  display: flex;
-  align-items: center;
-}
-
-.report-status__icon,
-.mediaplan-status__icon {
-  padding-right: 5px;
-}
-
-.mediaplan-mobile-status__icon {
-  padding-left: 5px;
-  vertical-align: middle;
-  display: inline-block;
-}
-
-.show-more-wrapper {
-  display: flex;
-  justify-content: flex-end;
-}
-
-.show-more-button {
-  background: none;
-  border: none;
-  color: main.$purple;
-  cursor: pointer;
-  font-size: 14px;
-  padding: 0;
+.desctop-hidden {
+  display: none;
 }
 
 @media screen and (max-width: 1200px) {
-  .files-container {
+  .files {
     width: 100%;
     height: 405px;
   }
 
-  .files-container__preview {
+  .files__preview {
     flex-direction: row;
     padding-top: 0;
   }
 
-  .files-container-desctop__wrapper {
+  .files__desktop-wrapper {
     flex-direction: row;
-  }
-
-  .header {
-    padding: 28px 0px 0px 20px;
-  }
-
-  .mediaplan,
-  .report {
-    width: 296px;
   }
 }
 
 @media screen and (max-width: 768px) {
-  .desctop-hidden {
+  .files-mobile {
     display: flex;
   }
 
